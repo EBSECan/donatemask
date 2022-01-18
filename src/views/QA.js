34a414c6d440a7f2fb677918884f044a0f35c328
Mekki MacAulay <mekki@mekki.ca>
2022-01-18 17:39:47 -0500

    Added first half of FAQ
    Added first half of FAQ

--- src/views/QA.js
+++ src/views/QA.js
@@ -1,4 +1,148 @@
 const QA = [
+  {
+  question: `There are lots of pandemic grifters out there, taking advantage of people when they are at their most vulnerable. How do I know this isn't a scam?
+`,
+  answer: `
+You are wise to do your due-diligence before donating money or sending your personal information (address) to a website! Good job!  You can verify that this is a real initiative by real people in several ways:
+
+1) Donate A Mask Project founder and Executive Director of Evidence-Based Social Enterprises Canda, Mekki MacAulay, has a deliberately public profile on the Internet that goes back two decades.  You can find everything about him on his LinkedIn Profile (https://www.linkedin.com/in/mekkim), his Twitter (https://twitter.com/mekki), his Facebook (https://facebook.com/mekki), and so on.  You can also email him at mekki@donatemask.ca with any concerns or questions.  
+
+Mekki takes his obligations as a Professional Engineer (PEng) registered with Professional Engineers Ontario (License #100134797) very seriously.  One of his obligations as a Professional Engineer is to always hold the public interest as paramount.  The word of a professional engineer is his oath.  You can look up Mekki's professional engineering registration at this site: https://www.peo.on.ca/directory (search for Mekki as first name--there's only one!).
+
+2) The Donate A Mask Project is an initiatve of Evidence-Based Social Enterprises Canada, a registered Charitable Organization*.  You can look up the organization with the Canada Revenue Agency to verify that it is, indeed a real charitable organization, at this location: https://apps.cra-arc.gc.ca/ebci/hacc/srch/pub/dsplyBscSrch?request_locale=en
+
+[*Chariable registration application in progress]
+
+3) Lastly, if you are a media organization who wants to validate that we are real and do due diligence before publication, please reach out to us at marketing@donatemask.ca and we will be happy to provide any documentation you may wish to validate who we are and what we do.
+
+4) The Donate A Mask Project Canada leverages the principles of the open source movement of transparency and auditability.  Our web page and activities are deliberately accessible to the general public via GitHub at this location: https://github.com/mekkim/donatemask - We would LOVE it if you want to reproduce this project using our resources in your own jurisdiction (USA? France? UK? Reach out to us! We'll help get you set up!)
+
+5) The following people have endorsed the Donate A Mask Project on pain of damage to their personal and professional reputations as being a real initiative! You can verify the authenticity of their endorsements by following the links on the endorsements to their own personal and professional websites where they have posted confirmation that this is real.`,
+},
+  
+  {
+  question: `When you say that you will provide "high-quality" masks to those who request them, what do you mean?
+`,
+  answer: `There is a lot of confusing messaging about masks, masking, and mask quality circulating during the pandemic.  When we say "high-quality" masks, what we mean is "NIOSH N95-rated respirators of various configutations or equivalent".  Technically, we are providing "respirators", not "masks", but since most people use the term "mask" in common discussions, we've adopted that term for simplicity of communication.
+
+We have three tiers of reference masks that we attempt to procure and distribute as part of this initiative.  Wherever possible, based on supply chain availability, we will provide the tier 1 masks.  In the event that they are not available, we will substitute tier 2 and 3 masks as necessary.  The lowest tier masks we provide will still be "NIOSH N95-rated or equivalent".
+
+The targetted tiers are as follows:
+
+Preferred (Tier 1): 3M Aura Model 1870+ Surgical Respirator: https://www.3mcanada.ca/3M/en_CA/p/d/v101271018/ 
+3M™ Aura™ Health Care Particulate Respirator and Surgical Mask,1870+, N95, 440 per case
+The 3M™ Aura™ Health Care Particulate Respirator and Surgical Mask 1870+ has comfortable inner materials and helps provide respiratory protection against certain airborne particles. The individually packaged flat fold design allows for convenient storage prior to use.
+	https://www.3mcanada.ca/3M/en_CA/p/d/v101271018/
+Also Preferred (Tier 1): 3M VFlex Healtcare / Surgical Respirator Model 1804 (regular or small): https://www.3mcanada.ca/3M/en_CA/p/d/v100553014/
+3M™ VFlex™ Healthcare Particulate Respirator and Surgical Mask, 1804, N95
+The 3M™ VFlex™ Healthcare Particulate Respirator and Surgical Mask helps provide respiratory protection against certain airborne particles. The folded respirator expands for a spacious feel and the pleats help increase surface area for ease of breathing.
+	https://www.3mcanada.ca/3M/en_CA/p/d/v100553014/
+When preferred models not available, VFlex & Aura non-surgical are next best (Tier 2): Models 9105 9205+ 9210+ (regular & small): https://www.3mcanada.ca/3M/en_CA/p/d/v000057779/
+https://www.3mcanada.ca/3M/en_CA/p/d/v101271017/
+https://www.3mcanada.ca/3M/en_CA/p/d/v101348002/
+3M™ VFlex™ Particulate Respirator, 9105, N95, regular
+The 3M™ VFlex™ Particulate Respirator 9105 has a unique folded design with V-shaped pleats that flex and expand with mouth movement for easier breathing and talking.
+	https://www.3mcanada.ca/3M/en_CA/p/d/v000057779/
+3M™ Aura™ Particulate Respirator 9205+, N95, 440/Case
+3M™ Aura™ Particulate Respirator is a lightweight, three-panel, flat-fold disposoable N95 respirator, that helps provide comfortable, reliable, and convenient worker respiratory protection against non-oil based particles, using a variety of innovative technologies and features. 3M's proprietary Advanced Electrostatic Media, filters dust and other particles while allowing for for easy breathing.
+	https://www.3mcanada.ca/3M/en_CA/p/d/v101271017/
+3M™ Aura™ Particulate Respirator 9210+/37192, N95, 240/Case
+3M™ Aura™ Particulate Respirator 9210+/37192 is a breakthrough in comfort and convenience. This three-panel, flat-fold disposable respirator's innovative design helps provide comfortable, reliable worker protection against non-oil based particles.
+	https://www.3mcanada.ca/3M/en_CA/p/d/v101348002/
+If none of preferred or second choice are available, then "standard" n95 oval masks are acceptable (Tier 3): 3M 8210  respirator (regular and small): https://www.3mcanada.ca/3M/en_CA/p/dc/v000062307/
+3M™ Particulate Respirator 8210, N95,
+3M™ Particulate Respirator 8210 is a classic disposable N95 particulate respirator designed to help provide comfortable, reliable respiratory protection against certain non-oil based particles.
+	https://www.3mcanada.ca/3M/en_CA/p/dc/v000062307/
+
+So that makes the 3 tiers for reference masks:
+3M VFlex/Aura surgical/medical grade or equivalent
+VFlex/Aura non-surgical/medical grade or equivalent
+Standard n95 oval mask or equivalent
+
+Where possible, we will source masks from Canadian manufacturers, suppliers, and distributors.
+
+Please note that while we will make every effort possible to ensure the quality of the masks we provide, we cannot and will not take on any responsibility or liability for their effectiveness for any or no purpose.  All products requested and received are used at your own risk.  Please see our Terms of Use for more details.  `,
+},
+  
+  {
+  question: ` Why don't you provide KN95 masks? Aren't they "high quality"?
+`,
+  answer: `You know your mask types! Good stuff!  Take a look at the comparison chart on this page:  https://smartairfilters.com/en/blog/whats-difference-between-n95-kn95-masks/
+
+Note the second last row "Force applied".  KN95 masks require <b>480% more</b> force applied in order to be as effective as N95 masks.  They are super, duper unpleasant to wear if they are worn correctly, and people are more likely to let their nose hang out, or pull it up or down, or not hook it behind their heads, and so on.  Further, there are all kinds of manufacturing problems as a result of this incredible force required, such as ear/head straps ripping when the full (correct!) force is applied.
+
+Given these issues, and the growing problem of counterfint KN95 masks appearing on the market, we decided to standardize on N95 masks whose NIOSH certifications we could validate ourself (3M as reference designs).`,
+},
+   
+  {
+  question: `bUt MaSKs aRE bAd FoR yoU!!!1!one!11!!`,
+  answer: `Please look at the title of our organization once more: "Evidence-Based Social Enterprises Canada".  We're not here to debate with anti-science zealots.  Don't want our free masks delivered right to your door using the labour of volunteers and the funding of genrous people who want society to be healthier?  Kool. Make room for those who want to protect themselves and the people they care about.  Don't let the website/app close button hit you on the way out!`,
+},
+   
+  {
+  question: `How many masks can I request?`,
+  answer: `This is a tricky question.  As we're just getting going, we know we're inevitably going to have to address this issue.  Right now, we don't have a good answer.  Please request what you feel you and those you are requesting for can use over a 60 day period at most (we may change this later).  Recent research has shown that you can sanitize and reuse N95 masks up to 25 times, so please clena and reuse them as much as you can so that there are more to go around.  Details here: https://www.ajicjournal.org/article/S0196-6553(21)00741-0/fulltext`,
+},
+   
+  {
+  question: `I'm requesting for an organization/group/other larger number. Can you help us out?
+`,
+  answer: `Maybe.  We'll be dealing with growing pains on an as-an-when-possible basis.  Case by case.  Sorry that there's no definitive answer at this time. We'll update the answer if/when we can make more commitments.`,
+},
+   
+  {
+  question: `If you get more requests for masks than donations, how do you prioritize who gets masks?`,
+  answer: `Good question.  The volunteer team has been noodling over this one.  The short answer is that we will be doing this on a case-by-case basis at first and will come up with more definitive policies as we grow.  Some guidelines we will use in fulfillment at present:
+
+1) Our goal is to reach the most vulnerable, including those who cannot otherwise access or afford high-quality masks; that will always be the top priority
+
+2) We will try to reach more people with fewer masks requested than fewer people with full orders of masks requested
+
+3) For-profit organizations are at or near the bottom
+
+4) Canada only (Reach out if you want to duplicate this project in another jurisdiction--We'll help you get started!)
+
+5) Areas that are covered by existing government (federal, provincial, municipal, other) jurisdiction/programs may get lower precendence.  e.g., we're unlikely to be able to send masks to all the schools that need to protect staff and students--The Provincial Governments really need to pick up the buck here.
+
+6) We may prioritize masks for certain groups/organizations based on donor requests.  e.g., if a donor sends us a donation with a request that it be spent getting masks to a specific identifiable group, we'll do our best to honour that request.
+
+7) Other factors as we get this sorted.  We're volunteers! Be patient with us, please! ^_^
+`,
+},
+   
+  {
+  question: `Why do you list "donations" in "masks" instead of dollars?  And why do you have the cost of "one mask" set at $2.50 (CAD)?`,
+  answer: `Great question!  We wanted to distinguish ourselves from other charitable initiatives by helping donors visualize exactly what they were doing.  By sizing donations in "number of masks", donors can visualize their friends, family and other members of society wearing those masks and directly understand the impact of their donation in terms of increased public health.  
+
+The value of $2.50 "per mask" was selected as a quasi-arbitrary number that incorporates factors such as mask cost, shipping costs, and organization costs, rounded upwards deliberately.  We, of course, pay nowhere near that amount per mask as we make volume purchases from suppliers.  However, this amount approximates the total costs of "getting a mask to a vulnerable person" when all things are factored in.
+
+Where there is extra money, more masks will be shipped with those funds; simple as that.  Everyone working on the Donate A Mask Project is an unpaid voluneer (whom we greatly appreciate!).  100% of donated funds go directly to the mission of putting masks in the mailboxes of requestors.  
+
+If you're a media organization who wishes to report on this project, please reach out to us at marketing@donatemask.ca and we will be happy to show you all of our financial statements so that you can validate how the money is used in this project.`,
+},
+   
+  {
+  question: `I am a large donor / donor organization who supports your missio nand wants to make a large contribution of funding or masks (3M Canad?  I'm looking at you!); how do I do that?`,
+  answer: `We're exited to collaborate with other like minded folk who understand the importance of putting high-quality masks directly in the hands of the most vulnerable in our society.  Reach out to us at donate@donatemask.ca and we can work out collaboration/donation/in-kind/product donation details.  Thank you!`,
+},
+   
+  {
+  question: `Will you issue a charitable donation receipt for my donation?`,
+  answer: `Yes, caveat: Our incorporation has been confirmed by Corporations Canada and our registered charity application is in progress.  Our aim is to have all donations received after January 14, 2022 of $25 or more (10+ masks) to be issued an official charitable donation receipt for the purposes of income tax in Canada.  However, please bear with us as we get up and running and please be understanding if our government paperwork takes longer than expected and we aren't able to issue charitable receipts until a later date (if we have problems with the paperwork, etc.).  Feel free to reach out to us at donate@donatemask.ca for status updates on the registered charity paperwork progress.`,
+},
+   
+  {
+  question: `You provide a lot of ways to donate money.  Which is best for the Donate A Mask Project?`,
+  answer: `Thanks for thinking about the flow of money.  As you no doubt know, payment processing can be expensive for charities.  We wanted to make a large range of donation payment options available to make donations as easy as possible, but, indeed, some cost more than others.
+
+The simplest and most cost-effective way for us to receive donations is via INTERAC email transfers sent to donate@donatemask.ca.  They will be auto-accepted and we will reply with confirmation of receipt to the email you used to send the donation.  In this manner, we receive 100% of the funds, with no fees.  Some other means are also cost effective, so please feel free to use the means that are best for you.`,
+},
+   
+  {
+  question: `I LOVE the Donate A Mask Project! I want to volunteer to help out!`,
+  answer: `Thanks! We LOVE passionate people and we welcome your help. Please reach out to us at volunteer@donatemask.ca and we'll find a way to put your specific skills and interests to work towards our mission of making high quality masks available to everyone with minimal fuss!`,
+},
+   
 {
   question: `Is there a mobile app?`,
   answer: `Not yet.  We're working on it and we'll let you know when it's live.  For now you can access the page using a mobile web browser.`,
@@ -111,6 +255,11 @@ const QA = [
 {
   question:` We would like to use some of this project's source code in our own projects but need a different license than the open source license provided. Will you consider different licensing?`,
   answer: `Maybe. Reach out to Mekki at mekki@donatemask.ca to discuss your needs.`,
+},
+    
+  {
+  question: `I have a question not in this FAQ. Who do I contact?`,
+  answer: `No doubt we haven't thought of all the questions you might have! Feel free to reach out to us at marketing@donatemask.ca with your questions. We'll udpate the FAQ accordingly.  Thank you!`,
 },
 ]
 
