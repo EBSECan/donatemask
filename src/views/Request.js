674bc33da6fcb9d053108d8cf8ab180569522240
Mekki MacAulay <mekki@mekki.ca>
2022-03-21 11:05:35 -0400

    Updated phrasing of request page
    Updated the phrasing of the request page.

--- src/views/Request.js
+++ src/views/Request.js
@@ -50,8 +50,8 @@ const Request = () => {
     <>
       <PageNavbar/>
       <Hero
-        heading="Request a mask."
-        body="In need of a face mask? Request some below from a kind stranger :)"/>
+        heading="Request masks."
+        body="In need of high-quality, N95-comparable, Health Canada-approved masks to protect you and your loved ones? Fill out the form below and our charity will send them to you for FREE."/>
         <Row className="d-flex justify-content-center no-margin pt-5">
           <RequestForm/>
         </Row>
