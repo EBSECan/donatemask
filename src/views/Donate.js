11b8430f74f31b5e5796ea354ed8285bd7569d7b
David Humphrey <david.andrew.humphrey@gmail.com>
2022-05-15 21:41:15 -0400

    Add /donate success Thank You page

--- src/views/Donate.js
+++ src/views/Donate.js
@@ -1,62 +1,33 @@
-/*!
+import React from "react";
+import { useLocation } from "react-router-dom";
+import { Row } from "reactstrap";
 
-=========================================================
-* Argon Design System React - v1.1.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-design-system-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-import React, {useState} from "react";
-// nodejs library that concatenates classes
-import classnames from "classnames";
-
-// reactstrap components
-import {
-  Badge,
-  Button,
-  Card,
-  CardBody,
-  CardImg,
-  FormGroup,
-  Input,
-  InputGroupAddon,
-  InputGroupText,
-  InputGroup,
-  Container,
-  Row,
-  Col
-} from "reactstrap";
-
-// core components
-import PageNavbar from "components/Navbars/PageNavbar.js";
-import DonateForm from "components/DonateForm.js"
-import RequestForm from "components/RequestForm.js"
-import Hero from 'components/Hero.js'
-import SimpleFooter from 'components/SimpleFooter.js';
-
+import PageNavbar from "components/Navbars/PageNavbar";
+import DonateForm from "components/DonateForm";
+import DonateThankYou from "components/DonateThankYou";
+import Hero from "components/Hero";
+import SimpleFooter from "components/SimpleFooter";
 
 const DonatePage = () => {
+  // If we're returning from a successful Stripe donation,
+  // the URL will include ?success=true
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const donationSuccess = params.get("success") === "true";
+
   return (
     <>
-    <PageNavbar/>
+      <PageNavbar />
       <Hero
         heading="Donate a mask"
-        body="Support the Donate A Mask Charity Project with a single or recurring donation. 100% of your donation goes towards helping vulnerable people."/>
+        body="Support the Donate A Mask Charity Project with a single or recurring donation. 100% of your donation goes towards helping vulnerable people."
+      />
       <Row className="d-flex justify-content-center no-margin pt-5">
-        <DonateForm/>
+        {donationSuccess ? <DonateThankYou /> : <DonateForm />}
       </Row>
-      <SimpleFooter/>
+      <SimpleFooter />
     </>
   );
-}
+};
 
 export default DonatePage;
