6e56fd4cd5225ac054cb23bf72a23b606d2ae066
David Humphrey <david.andrew.humphrey@gmail.com>
2022-08-10 12:44:04 -0400

    Add code to generate Request Report

--- server/db/mask-requests.js
+++ server/db/mask-requests.js
@@ -24,14 +24,10 @@ module.exports.get = () => {
     .collection("maskrequests")
     .find({})
     .project({
-      requestorType: 0,
       organizationName: 0,
       organizationType: 0,
       name: 0,
       email: 0,
-      address: 0,
-      postal: 0,
-      province: 0,
     })
     .toArray();
 };
--- server/util.js
+++ server/util.js
@@ -1,13 +1,25 @@
 // Get a positive, integer value for a given number.
 // Non-numbers will return 0 (e.g., null)
-const toInt = (value) => Math.abs(value|0);
+const toInt = (value) => Math.abs(value | 0);
+
+// Turn a decimal value into a formatted percent
+const toPercent = (value) => {
+  if (value === 1) {
+    return "100%";
+  }
+  if (value === 0) {
+    return "0%";
+  }
+  return `${(value * 100).toFixed(2)}%`.replace(/.00%$/, '%');
+};
 
 // Allow overriding the web URL, but default to the public domain
-const webUrl = process.env.WEB_URL || 'https://donatemask.ca';
+const webUrl = process.env.WEB_URL || "https://donatemask.ca";
 
 // Allow overriding the Stripe Price ID for testing, but use the default otherwise
-const priceId = process.env.STRIPE_PRICE_ID || 'price_1KzoQkCOL3X1doeXOy2OfORX'
+const priceId = process.env.STRIPE_PRICE_ID || "price_1KzoQkCOL3X1doeXOy2OfORX";
 
 module.exports.toInt = toInt;
+module.exports.toPercent = toPercent;
 module.exports.priceId = priceId;
 module.exports.webUrl = webUrl;
